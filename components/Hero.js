d32f5adb3c37ce14aef702cb4ff558aafb0923ee
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2022-05-22 11:32:25 -0500

    corregir class por className

--- components/Hero.js
+++ components/Hero.js
@@ -11,41 +11,41 @@ export default function Hero({
 }) {
   return (
     <>
-      <div class="hero rounded-lg py-8 m-4 md:mx-0 bg-primary shadow-lg">
+      <div className="hero rounded-lg py-8 m-4 md:mx-0 bg-primary shadow-lg">
         {imagePosition === "" ? (
-          <div class="hero-content text-center">
-            <div class="max-w-md">
-              <h1 class="text-5xl font-bold text-accent">{title}</h1>
-              <p class="py-6 text-accent">{text}</p>
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-5xl font-bold text-accent">{title}</h1>
+              <p className="py-6 text-accent">{text}</p>
               {link && (
                 <Link href={link}>
-                  <a class="btn btn-primary">{linkTitle}</a>
+                  <a className="btn btn-primary">{linkTitle}</a>
                 </Link>
               )}
             </div>
           </div>
         ) : imagePosition === "left" ? (
-          <div class="hero-content flex-col lg:flex-row gap-10">
+          <div className="hero-content flex-col lg:flex-row gap-10">
             <img src={image} className="max-w-sm rounded-lg" />
-            <div class="max-w-md">
-              <h1 class="text-5xl font-bold text-accent">{title}</h1>
-              <p class="py-6 text-accent">{text}</p>
+            <div className="max-w-md">
+              <h1 className="text-5xl font-bold text-accent">{title}</h1>
+              <p className="py-6 text-accent">{text}</p>
               {link && (
                 <Link href={link}>
-                  <a class="btn btn-primary">{linkTitle}</a>
+                  <a className="btn btn-primary">{linkTitle}</a>
                 </Link>
               )}
             </div>
           </div>
         ) : imagePosition === "right" ? (
-          <div class="hero-content flex-col lg:flex-row-reverse gap-10">
+          <div className="hero-content flex-col lg:flex-row-reverse gap-10">
             <img src={image} className="max-w-sm rounded-lg" />
-            <div class="max-w-md">
-              <h1 class="text-5xl font-bold text-accent">{title}</h1>
-              <p class="py-6 text-accent">{text}</p>
+            <div className="max-w-md">
+              <h1 className="text-5xl font-bold text-accent">{title}</h1>
+              <p className="py-6 text-accent">{text}</p>
               {link && (
                 <Link href={link}>
-                  <a class="btn btn-primary">{linkTitle}</a>
+                  <a className="btn btn-primary">{linkTitle}</a>
                 </Link>
               )}
             </div>
