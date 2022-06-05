c9e1315c06754b62065557963ad542867b0ed23f
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2022-06-05 14:09:47 -0500

    modificar hero principal y optimizar fondo

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Logo from "../assets/images/LogoBigleBlanco.svg";
 export default function Home() {
   const particlesInit = async (main) => {
     await loadFull(main);
@@ -15,36 +16,14 @@ export default function Home() {
       <Head>
         <title>Bigle | Inicio</title>
         <meta name="description" content="Soluciones a la medida" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <div className="bg-gradient-to-b from-primary to-secondary bg-opacity-50 hero py-10 bg-primary shadow-lg h-screen relative">
         <Particles
           id="tsparticles"
           options={{
-            background: {
-              image: "",
-              position: "",
-              repeat: "",
-              size: "",
-              opacity: 1,
-            },
             style: {
               position: "absolute",
-            },
-            backgroundMask: {
-              composite: "destination-out",
-              cover: {
-                color: {
-                  value: "#fff",
-                },
-                opacity: 1,
-              },
-              enable: false,
-            },
-            fullScreen: {
-              enable: true,
-              zIndex: 0,
             },
             fpsLimit: 120,
             interactivity: {
@@ -52,12 +31,6 @@ export default function Home() {
                 onClick: {
                   enable: true,
                   mode: "push",
-                },
-                onDiv: {
-                  selectors: [],
-                  enable: false,
-                  mode: [],
-                  type: "circle",
                 },
                 onHover: {
                   enable: true,
@@ -67,10 +40,10 @@ export default function Home() {
               },
               modes: {
                 push: {
-                  quantity: 3,
+                  quantity: 1,
                 },
                 repulse: {
-                  distance: 100,
+                  distance: 80,
                   duration: 0.4,
                 },
               },
@@ -84,10 +57,7 @@ export default function Home() {
                 distance: 150,
                 enable: true,
                 opacity: 0.5,
-                width: 1,
-              },
-              collisions: {
-                enable: true,
+                width: 0.5,
               },
               move: {
                 direction: "none",
@@ -96,37 +66,51 @@ export default function Home() {
                   default: "bounce",
                 },
                 random: false,
-                speed: 1,
+                speed: 0.8,
                 straight: false,
               },
               number: {
                 value: 100,
               },
               opacity: {
-                value: 0.5,
+                value: 0.4,
               },
               shape: {
                 type: "circle",
               },
               size: {
-                value: { min: 1, max: 5 },
+                value: { min: 1, max: 4 },
               },
             },
             pauseOnBlur: true,
             pauseOnOutsideViewport: true,
-            responsive: [],
+            responsive: [
+              {
+                maxWidth: 800,
+                options: {
+                  particles: {
+                    number: {
+                      value: 40,
+                    },
+                    size: {
+                      value: { min: 1, max: 4 },
+                    },
+                  },
+                },
+              },
+            ],
             detectRetina: true,
           }}
           init={particlesInit}
           loaded={particlesLoaded}
         />
         <div className="hero-content flex-col lg:flex-row gap-10">
-          <img src="/LogoBigleBlanco.svg" className="max-w-xs" />
+          <Logo className="w-full" />
           <div className="max-w-md">
             <h1 className="text-5xl font-bold text-accent">
               Paquetes de servicios online para tu empresa
             </h1>
-            <p className="py-6 text-xl text-accent">
+            <p className="py-6 text-xl text-white">
               Nuestro equipo está dispuesto a ayudarte
             </p>
           </div>
