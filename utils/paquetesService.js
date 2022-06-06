d7e9e76337884855fafa87673cc19a34d31e1858
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2022-06-05 20:44:50 -0500

    corregir link api 2

--- utils/paquetesService.js
+++ utils/paquetesService.js
@@ -1,17 +1,21 @@
 export async function getPaquetes() {
-  const res = await fetch("/api/paquetes");
+  const res = await fetch("https://bigle-com-co.vercel.app/api/paquetes");
   const data = await res.json();
   return data;
 }
 
 export async function getPaquete(slug) {
-  const res = await fetch(`/api/paquetes/${slug}`);
+  const res = await fetch(
+    `https://bigle-com-co.vercel.app/api/paquetes/${slug}`
+  );
   const data = await res.json();
   return data;
 }
 
 export async function getPaquetesByCategory(category) {
-  const res = await fetch(`/api/paquetes/${category}`);
+  const res = await fetch(
+    `https://bigle-com-co.vercel.app/api/paquetes/${category}`
+  );
   const data = await res.json();
   return data;
 }
