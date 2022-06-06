941008c8089affeb50bbfb414102d9a7f0513a98
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2022-06-05 20:43:35 -0500

    corregir links api

--- utils/paquetesService.js
+++ utils/paquetesService.js
@@ -1,17 +1,17 @@
 export async function getPaquetes() {
-  const res = await fetch("http://localhost:3000/api/paquetes");
+  const res = await fetch("/api/paquetes");
   const data = await res.json();
   return data;
 }
 
 export async function getPaquete(slug) {
-  const res = await fetch(`http://localhost:3000/api/paquetes/${slug}`);
+  const res = await fetch(`/api/paquetes/${slug}`);
   const data = await res.json();
   return data;
 }
 
 export async function getPaquetesByCategory(category) {
-  const res = await fetch(`http://localhost:3000/api/paquetes/${category}`);
+  const res = await fetch(`/api/paquetes/${category}`);
   const data = await res.json();
   return data;
 }
