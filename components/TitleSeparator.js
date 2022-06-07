4aabfc7a95ba1685366f0a9b6a23f27708744e3b
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2022-06-06 20:10:07 -0500

    aumentar margen

--- components/TitleSeparator.js
+++ components/TitleSeparator.js
@@ -1,7 +1,7 @@
 export default function TitleSeparator({ title, text }) {
   return (
     <>
-      <div className="flex flex-col items-center justify-center text-center my-10">
+      <div className="flex flex-col items-center justify-center text-center my-20">
         <h1 className="text-4xl font-bold text-primary">{title}</h1>
         <p className="text-primary font-medium my-2">{text}</p>
       </div>
