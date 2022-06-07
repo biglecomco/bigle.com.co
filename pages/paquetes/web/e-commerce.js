354546f57703aa77c76945208f83b5f406ea3999
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2022-06-06 20:12:17 -0500

    crear paginas base paquetes

---
+++ pages/paquetes/web/e-commerce.js
@@ -0,0 +1,26 @@
+import Layout from "../../../components/Layout";
+import Paquete from "../../../components/Paquete";
+import { paquetes } from "../../../data/paquetes";
+
+export default function ECommerce() {
+  return (
+    <>
+      <Layout>
+        {paquetes
+          .filter((paquete) => paquete.slug === "e-commerce")
+          .map((paquete) => (
+            <Paquete
+              title={paquete.title}
+              link={paquete.slug}
+              miniDescription={paquete.miniDescription}
+              description={paquete.description}
+              caracteristicas={paquete.caracteristicas}
+              imagen1=""
+              imagen2=""
+              showAs="page"
+            />
+          ))}
+      </Layout>
+    </>
+  );
+}
