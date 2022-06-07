9256cabcbbeb43f61d53a8d0a2781573bac44653
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2022-06-06 20:11:49 -0500

    eliminar api y crear cards de paquetes de redes sociales

--- pages/paquetes.js
+++ pages/paquetes.js
@@ -4,9 +4,9 @@ import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 import TitleSeparator from "../components/TitleSeparator";
 import Paquete from "../components/Paquete";
-import { getPaquetes } from "../utils/paquetesService";
+import { paquetes } from "../data/paquetes";
 
-export default function Paquetes({ paquetes }) {
+export default function Paquetes() {
   return (
     <>
       <Head>
@@ -24,23 +24,44 @@ export default function Paquetes({ paquetes }) {
           text="Desde una landing page hasta un e-commerce, nosotros lo haremos."
         />
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-3 sm:p-0">
-          {paquetes &&
-            paquetes.map((paquete) => (
-              <Paquete key={paquete.id} paquete={paquete} showAs="card" />
+          {paquetes
+            .filter((card) => card.category === "web")
+            .map((card) => (
+              <Paquete
+                key={card.id}
+                category={card.category}
+                title={card.title}
+                slug={card.slug}
+                image={card.image}
+                description={card.description}
+                minidescription={card.minidescription}
+                caracteristicas={card.caracteristicas}
+                showAs="card"
+              />
+            ))}
+        </div>
+        <TitleSeparator
+          title="Redes sociales y publicidad"
+          text="Manejamos las redes de tu negocio y las expandimos."
+        />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-3 sm:p-0">
+          {paquetes
+            .filter((card) => card.category === "redes-sociales")
+            .map((card) => (
+              <Paquete
+                key={card.id}
+                category={card.category}
+                title={card.title}
+                slug={card.slug}
+                image={card.image}
+                description={card.description}
+                minidescription={card.minidescription}
+                caracteristicas={card.caracteristicas}
+                showAs="card"
+              />
             ))}
         </div>
       </Layout>
     </>
   );
-}
-
-export async function getStaticProps() {
-  const res = await getPaquetes();
-
-  return {
-    props: {
-      paquetes: res,
-    },
-    revalidate: 60, // In seconds
-  };
 }
